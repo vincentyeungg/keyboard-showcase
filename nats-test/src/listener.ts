7370e7ae27e0563b0e8d7a88ba24f929196fe94b
vincentyeungg <vincentyeung0809@gmail.com>
2022-03-25 00:10:44 -0400

    working on mini projects with NATS

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,8 +10,19 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('listener connected to NATS');
 
+    stan.on('close', () => {
+        console.log('NATS connection closed!');
+        // end this process, don't do anything else
+        process.exit();
+    });
+
+    // to set different options, we can use NATS method of chaining different method calls
+    const options = stan
+        .subscriptionOptions()
+        .setManualAckMode(true);
+
     // create a subscription to listen to a specific channel
-    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
+    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group', options);
 
     // listen for 'message' event from subscription to specific channel
     subscription.on('message', (msg: Message) => {
@@ -20,5 +31,11 @@ stan.on('connect', () => {
         if (typeof data === 'string') {
             console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
         }
+
+        // manually acknowledge if using manual ack mode
+        msg.ack();
     });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
